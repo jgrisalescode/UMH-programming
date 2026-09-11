1c41eebeda27c89c9eb7cc41006bdbe2e4b59acf
Julian Grisales <jgrisales@gmail.com>
2026-09-11 23:02:23 +0200

    Bloque1: Ejercicio 2

---
+++ ejercicios_bloque1/02_detective_de_nombres.py
@@ -0,0 +1,7 @@
+# 3profesor => No válido, inicia con un dígito.
+# nota_final => Válido, buenas prácticas en el nombrado de variables.
+# __init => Válido, pero no hace parte de las buenas prácticas en Python para nombrar variables
+# for => No válido, palabra reservada del lenguaje
+# MediaClase => Válido, pero no hace parte de las buenas prácticas en Python para nombrar variables
+# nota-parcial => No válido, se confundiría con una resta
+# Aprobado2024 => Válido, pero no hace parte de las buenas prácticas en Python para nombrar variables
