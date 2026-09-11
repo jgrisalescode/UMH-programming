6e836b7766c67f2f07774702ed2499ed809a358a
Julian Grisales <jgrisales@gmail.com>
2026-09-11 23:13:22 +0200

    Bloque1: Ejercicio 4

---
+++ ejercicios_bloque1/04_adivina_el_tipo.py
@@ -0,0 +1,13 @@
+# 3 + 2 => int
+# 3 / 2 => float
+# 3 // 2 => int
+# "3" + "2" => str
+# 3 != 2 => bool
+# "3.5" => str
+
+print(type(3 + 2))
+print(type(3 / 2))
+print(type(3 // 2))
+print(type("3" + "2"))
+print(type(3 != 2))
+print(type("3.5"))
