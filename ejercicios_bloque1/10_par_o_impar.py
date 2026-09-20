bf06a2be759cbe0f68a3c5b720a53279b39a4c28
Julian Grisales <jgrisales@gmail.com>
2026-09-20 13:23:10 +0200

    Bloque1: Ejercicio 10

---
+++ ejercicios_bloque1/10_par_o_impar.py
@@ -0,0 +1,9 @@
+numero: int = 25
+es_par: bool = numero % 2 == 0
+
+print(f"¿El numero {numero} es par? {es_par}")
+
+numero = 46
+es_par: bool = numero % 2 == 0
+
+print(f"¿El numero {numero} es par? {es_par}")
