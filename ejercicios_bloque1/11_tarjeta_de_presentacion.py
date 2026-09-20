41336480c99e402d6b78bcd509d53775da5b0415
Julian Grisales <jgrisales@gmail.com>
2026-09-20 13:43:05 +0200

    Bloque1: Ejercicio 11

---
+++ ejercicios_bloque1/11_tarjeta_de_presentacion.py
@@ -0,0 +1,5 @@
+name: str = input("Ingresa tu nombre: ")
+age: str = input("Ingresa tu edad: ")
+city: str = input("Ingresa tu ciudad: ")
+
+print(f"Hola, soy {name}, tengo {age} años y vivo en {city}")
