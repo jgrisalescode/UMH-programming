7a137bb3519de558ccffd4459edcbac3142a7783
Julian Grisales <jgrisales@gmail.com>
2026-09-20 14:06:20 +0200

    Bloque1: Ejercicio 14

---
+++ ejercicios_bloque1/14_conversion_de_temperatura.py
@@ -0,0 +1,4 @@
+temperatura_en_celsius: float = float(input("Ingresa la temperatura en grados celsius: "))
+temperatura_en_fahrenheit: float = (temperatura_en_celsius * 9 / 5 + 32)
+
+print(f"Grados Celsius: {temperatura_en_celsius}\nGrados Fahrenheit: {temperatura_en_fahrenheit}")
