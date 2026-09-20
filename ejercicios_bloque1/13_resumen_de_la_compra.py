0bf088f84e329400096d8b4e5518d3164a013800
Julian Grisales <jgrisales@gmail.com>
2026-09-20 14:28:38 +0200

    Bloque1: Ejercicio 17

--- ejercicios_bloque1/13_resumen_de_la_compra.py
+++ ejercicios_bloque1/13_resumen_de_la_compra.py
@@ -1,7 +1,13 @@
+"""
+Ejercicios Bloque 1: Fundamentos de Programación
+Simulamos la lista de la compra
+"""
+
 verduras: float = float(input("Precio de las verduras: "))
 frutas: float = float(input("Precio de las frutas: "))
 carnes: float = float(input("Precio de las carnes: "))
 
 total_compra: float = verduras + frutas + carnes
 
+# {total_compra:.2f} presenta el valor total con 2 dígitos en la parte decimal.
 print(f"Total compra: {total_compra:.2f}€")
