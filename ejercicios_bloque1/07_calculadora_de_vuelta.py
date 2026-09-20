7907ce705bab72212d1e683fd45381801cdb8437
Julian Grisales <jgrisales@gmail.com>
2026-09-20 12:12:33 +0200

    Bloque1: Ejercicio7

---
+++ ejercicios_bloque1/07_calculadora_de_vuelta.py
@@ -0,0 +1,12 @@
+precio_producto: float = 67.99
+precio_pagado: float = 80.00
+vuelta: float = precio_pagado - precio_producto
+
+print(f"Valor Producto: {precio_producto}€\nValor Pagado: {precio_pagado}€\nVuelta: {vuelta}")
+
+# Este es el print de este fichero
+# Valor Producto: 67.99€
+# Valor Pagado: 80.0€
+# Vuelta: 12.010000000000005
+
+# Me parece muy curioso el valor de Vuelta.
