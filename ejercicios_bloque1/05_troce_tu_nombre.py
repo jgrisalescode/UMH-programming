a6b0b729656e7370a08219d4d51152cfa085938a
Julian Grisales <jgrisales@gmail.com>
2026-09-20 11:16:59 +0200

    Bloque1: Ejercicio 5

---
+++ ejercicios_bloque1/05_troce_tu_nombre.py
@@ -0,0 +1,5 @@
+my_name: str = "Julian Grisales"
+print("Primer letra: " + my_name[0])
+print("Última letra: " + my_name[-1])
+print(f"Longitud de mi nombre: {len(my_name)}")
+print("Primeras 3 letras: " + my_name[0:3])
